7174d7abc52e33ca090846908837af84dff60b10
juandaabril <juandaabril@gmail.com>
2024-09-16 20:05:43 -0500

    fix: broken test

--- springboot-data-mongodb/src/test/java/com/juandaabril/springboot_data_mongodb/controller/TodoControllerTest.java
+++ springboot-data-mongodb/src/test/java/com/juandaabril/springboot_data_mongodb/controller/TodoControllerTest.java
@@ -1,20 +1,12 @@
 package com.juandaabril.springboot_data_mongodb.controller;
 
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.utility.DockerImageName;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @WebMvcTest
 @Testcontainers
 class TodoControllerTest {
 
 
-    @Container
-    MongoDBContainer mongoDbContainer() {
-        return new MongoDBContainer(DockerImageName.parse("mongo:latest"));
-    }
 
 }
